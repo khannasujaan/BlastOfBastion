c9f90914075247c3ee14acbe541a29dbf3cf3785
khannasujaan <khannasujaan@gmail.com>
2026-06-08 04:53:52 +0530

    feat: add docker composer and server setup

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/khannasujaan/BlastOfBastion
 
 go 1.26.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: No .env file found")
+	}
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Server is healthy")
+	})
+
+	port := os.Getenv("PORT")
+	fmt.Println("Loading Server at PORT", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("Error in Server loading")
+	}
 }
